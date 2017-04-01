0e7010dd8d3633173ee8300a20f83e59636aeb3a
rmit-s3569974-Ning-Nan <mac@macdeMacBook-Pro-2.local>
2017-04-01 16:32:07 +1100

    make register JUNIT test

---
+++ test/bookingsystem/userRegisterTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookingsystem;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class userRegisterTest {
+
+    public userRegisterTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        try {
+            bdb.setup();
+            user user = new user("Ning", "pater", "9925", "9925", "Boxhill",
+                    "0405727777", "nanning@gmail.com");
+            System.out.println("1");
+
+        } catch (SQLException e) {
+            fail(e.getMessage());
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws SQLException {
+        bdb.iuQuery("DELETE FROM customers WHERE name = 'Ning'");
+          System.out.println("3");
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() throws SQLException {
+        ResultSet rs = bdb.selectQuery(
+                "SELECT * from customers WHERE username='pater'");
+        assert(rs.getString("name").equals("Ning"));
+        assert(rs.getString("phonenumber").equals("0405727777"));
+        assert(rs.getString("password").equals("9925"));
+        assert(rs.getString("address").equals("Boxhill"));
+        assert(rs.getString("email").equals("nanning@gmail.com"));
+          System.out.println("2");
+        
+    }
+
+}
