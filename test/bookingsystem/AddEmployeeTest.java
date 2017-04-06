47a91834eaa9aa2696f9a42f29f6fdd3b48e7e66
rmit-s3569974-Ning-Nan <s3569974@student.rmit.edu.au>
2017-04-06 23:08:24 +1000

    add Employee test

---
+++ test/bookingsystem/AddEmployeeTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookingsystem;
+
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User
+ */
+public class AddEmployeeTest {
+    
+    public AddEmployeeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+          try {
+            bdb.setup();
+            business.currBusiness = new business(1, "rbusiness", "rbpass");
+
+        } catch (SQLException e) {
+            fail(e.getMessage());
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+    @Test
+    public void testMethod() {
+    
+        business.currBusiness.addEmployee("Java");
+        
+        employee test = business.currBusiness.getEmployee(4);
+        
+        assert(test.getBusinessID() == 1);
+        assert(test.getId() == 4);
+        assert(test.getName().equals("Java"));
+        
+    }
+}
