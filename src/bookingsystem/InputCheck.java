3246b8f09a9487ca506dd47fae26ff021bcb502d
rmit-s3569974-Ning-Nan <s3569974@student.rmit.edu.au>
2017-05-08 20:03:03 +1000

    add javadoc for input check class

--- src/bookingsystem/InputCheck.java
+++ src/bookingsystem/InputCheck.java
@@ -8,14 +8,21 @@ package bookingsystem;
 import java.util.regex.*;
 
 /**
+ * A class contains functions to check user input.
  *
- * @author User
  */
 public class InputCheck {
 
     public static InputCheck check;
     private String error;
 
+    /**
+     * Check username input.
+     *
+     * @param username username input
+     * @throws Exception
+     * @return success or not for checking input
+     */
     public boolean checkUserName(String username) throws Exception {
         String check1 = "^[a-zA-Z]\\w{6,12}$";
 
@@ -32,6 +39,13 @@ public class InputCheck {
 
     }
 
+    /**
+     * Check password input.
+     *
+     * @param password password input
+     * @throws Exception
+     * @return success or not for checking input
+     */
     public boolean checkPassword(String password) throws Exception {
         String check1 = "^[a-zA-Z]\\w{6,12}$";
 
@@ -48,6 +62,13 @@ public class InputCheck {
 
     }
 
+    /**
+     * Check short name input.
+     *
+     * @param name name input
+     * @throws Exception
+     * @return success or not for checking input
+     */
     public boolean checkShortName(String name) throws Exception {
 
         String check1 = "^([A-Z][A-Za-z ,.'`-]{3,30})$";
@@ -65,6 +86,13 @@ public class InputCheck {
 
     }
 
+    /**
+     * Check long string input.
+     *
+     * @param address address input
+     * @throws Exception
+     * @return success or not for checking input
+     */
     public boolean checkLong(String address) throws Exception {
         String check1 = "^[A-Za-z0-9]\\w{2,40}$";
 
@@ -83,6 +111,13 @@ public class InputCheck {
         return true;
     }
 
+    /**
+     * Check email input.
+     *
+     * @param email email input
+     * @throws Exception
+     * @return success or not for checking input
+     */
     public boolean checkEmail(String email) throws Exception {
         String check1 = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
 
@@ -101,8 +136,15 @@ public class InputCheck {
 
     }
 
+    /**
+     * Check phone no. input.
+     *
+     * @param phone phone number input
+     * @throws Exception
+     * @return success or not for checking input
+     */
     public boolean checkPhone(String phone) throws Exception {
-        
+
         String check1 = "^\\d{10}$";
 
         Pattern regex = Pattern.compile(check1);
